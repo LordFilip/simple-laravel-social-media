783394a35f39da7a021d9c583ef84e925ee4f8d4
Filip <filipivanovic59@gmail.com>
2024-08-18 07:55:35 +0200

    added layouts

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.layout')
+
+
+@section('content')
+
+
+        <h1>Terms</h1>
+        <div>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum impedit vitae inventore voluptas delectus, blanditiis a veniam soluta, fugit modi eos fuga? Magni magnam laboriosam veniam provident hic aliquam minima.
+        </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',[DashboardController::class,'index']);
-Route::get('/profile',[ProfileController::class,'index']);
+Route::get('/terms',function (){
+    return view('terms');
+});
 
 
